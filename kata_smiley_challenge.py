99ff6b4dd838e6db54e6c5511d88cbdb71af5ce9
“wail <wailabuahmad@gmail.com>
2019-03-19 16:53:29 +0100

    kata Challenge week 6

---
+++ kata_smiley_challenge.py
@@ -0,0 +1,10 @@
+def count_smileys(arr):
+    count = 0
+    for item in arr :
+        if len(item) ==2 :
+            if (item[0] == ":" or item [0] == ";") and (item[-1] == ")" or item[-1] == "D")  :
+                count +=1
+        elif len(item) <4 :
+            if (item[0] == ":" or item [0] == ";") and (item[-1] == ")" or item[-1] == "D") and (item[1] =="" or item[1] =="-" or item[1] == "~"):
+                count +=1
+    return count
